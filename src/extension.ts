ef354bf6c559161e967c199a547c638358d64676
Georgi Gardev <georgi.gardev@leanplum.com>
2018-09-13 20:59:28 -0400

    Add openRelatedFileWithExtension

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 'use strict';
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 import { RelatedFiles } from './relatedFiles';
 import { ExtensionSettings } from './settings';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('extension.openRelatedFiles', async () => {
+  let openRelatedFiles = vscode.commands.registerCommand('extension.openRelatedFiles', async () => {
     if (vscode.window && vscode.window.activeTextEditor) {
       const filePath = vscode.window.activeTextEditor.document.fileName;
 
@@ -41,7 +42,28 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  context.subscriptions.push(disposable);
+  let openRelatedFilesWithExtension = vscode.commands.registerCommand(
+    'extension.openRelatedFilesWithExtension',
+    async (withExtension: string) => {
+      if (vscode.window && vscode.window.activeTextEditor) {
+        const filePath = vscode.window.activeTextEditor.document.fileName;
+        const relatedFiles = new RelatedFiles(filePath);
+
+        const fileToOpen = relatedFiles.getRelatedFile(withExtension);
+        if (fs.existsSync(fileToOpen)) {
+          const document = await vscode.workspace.openTextDocument(fileToOpen);
+          vscode.window.showTextDocument(document);
+        } else {
+          vscode.window.showInformationMessage("File doesn't exist.");
+        }
+      } else {
+        vscode.window.showInformationMessage('You have to open a file first');
+      }
+    }
+  );
+
+  context.subscriptions.push(openRelatedFiles);
+  context.subscriptions.push(openRelatedFilesWithExtension);
 }
 
 export function deactivate() {}
